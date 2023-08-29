0997c1564b7fc18893efd3daca9d06cbdea9002f
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-08-29 21:34:52 +0000

    Primeiro programa em java

--- Licao01/HelloWorld.java
+++ Licao01/HelloWorld.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Recebendo a entrada do usuario.
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("Insira seu nome: ");
