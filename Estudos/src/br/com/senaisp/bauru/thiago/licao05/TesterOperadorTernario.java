e1cbe7c33098911717d77c41b7583a1f95654fb8
Thi <tthiagovinnicius@gmail.com>
2023-09-05 21:16:35 -0300

    Operador Tenario

--- Estudos/src/br/com/senaisp/bauru/thiago/licao05/TesterOperadorTernario.java
+++ Estudos/src/br/com/senaisp/bauru/thiago/licao05/TesterOperadorTernario.java
@@ -1,3 +1,20 @@
-public class TesterOperadorTernario {
+package br.com.senaisp.bauru.thiago.licao05;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Thiago
+ * 
+ */
+public class TesterOperadorTenario {
     
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe sua idade: ");
+        int idade = scan.nextInt();
+
+        System.out.printf("Você %s de idade!%n", idade >= 18 ? "é maior" : "é menor");
+    }
 }
